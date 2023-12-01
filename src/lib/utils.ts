79f297884e8d9ba0fb96156ce056b8bf2f16ac9c
SlickYeet <fglp.cm@gmail.com>
2023-12-01 01:08:59 -0800

    updated utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,7 +9,8 @@ export function cn(...inputs: ClassValue[]) {
 export function absoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
   if (process.env.VERCEL_URL) return `https://www.famlam.ca${path}`;
-  return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+  return `http://localhost:${process.env.PORT ?? 3000}${path}`; // dev
+  // return `https://www.famlam.ca}${path}`; // prod
 }
 
 export function constructMetadata({
