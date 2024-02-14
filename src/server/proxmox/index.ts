b8efcbbec7a91cfcfe03b3951797c903f50a7663
SlickYeet <lasse@famlam.ca>
2024-02-14 15:42:45 -0800

    fixed serverType breaking fetch (hopefully)

--- src/server/proxmox/index.ts
+++ src/server/proxmox/index.ts
@@ -8,7 +8,7 @@ interface Response {
 }
 
 const fetchServerData = async (type: serverType) => {
-  const url = `${process.env.PROXMOX_API_URL}nodes/pve/${type ? type : "lxc"}`;
+  const url = `${process.env.PROXMOX_API_URL}nodes/pve/${!type ? ("lxc" as serverType) : type}`;
 
   const res = (await fetchAccessTicket()) as Response;
 
