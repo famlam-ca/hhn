08583765a20673c2e09238f87534a76f6bd50ff6
SlickYeet <lasse@famlam.ca>
2024-10-15 15:37:08 -0700

    Update Prettier configuration and add new environment variable in .env.example

--- src/lib/services/user-service.ts
+++ src/lib/services/user-service.ts
@@ -1,21 +1,21 @@
-"use server";
+"use server"
 
-import { user } from "@prisma/client";
-import { compare, hash } from "bcrypt";
-import { revalidatePath } from "next/cache";
-import { z } from "zod";
+import { User } from "@prisma/client"
+import { compare, hash } from "bcrypt"
+import { revalidatePath } from "next/cache"
+import { z } from "zod"
 
-import { db } from "@/lib/db";
-import { validateSession } from "@/lib/lucia";
-import { createUserSession } from "@/lib/services/auth-service";
+import { db } from "@/lib/db"
+import { validateSession } from "@/lib/lucia"
+import { createUserSession } from "@/lib/services/auth-service"
 import {
   sendNewVerificationEmail,
   sendPasswordWasResetEmail,
-} from "@/lib/services/email-service";
+} from "@/lib/services/email-service"
 import {
   EditAccountSchema,
   ResetPasswordSchemaStep2,
-} from "@/types/user-schema";
+} from "@/types/user-schema"
 
 export const getUser = async (
   identifier: { email: string } | { username: string } | { userId: string },
@@ -43,43 +43,43 @@ export const getUser = async (
             theme: true,
           }
         : undefined,
-  });
+  })
   if (!dbUser) {
     return {
       success: false,
       message: "User not found",
-    };
+    }
   }
 
   return {
     success: true,
     user: dbUser,
-  };
-};
+  }
+}
 
 export const getFakeUser = async () => {
-  const randomNum = Math.floor(Math.random() * 10000);
+  const randomNum = Math.floor(Math.random() * 10000)
   const user = {
     id: `userId-${randomNum}`,
     display_name: `User-${randomNum}`,
     username: `user-${randomNum}`,
     role: "user",
-  };
+  }
 
-  return user;
-};
+  return user
+}
 
 export const getAllUsers = async () => {
-  const dbUser = await db.user.findMany();
+  const dbUser = await db.user.findMany()
   if (!dbUser) {
     return {
       success: false,
       message: "No users found",
-    };
+    }
   }
 
-  return dbUser;
-};
+  return dbUser
+}
 
 // TODO: implement user socials
 // export const getUserSocials = async (id: string) => {
@@ -92,17 +92,17 @@ export const getAllUsers = async () => {
 //   return dbSocials;
 // };
 
-export const updateUser = async (values: Partial<user>) => {
-  const { user } = await getUser({ userId: values.id! });
+export const updateUser = async (values: Partial<User>) => {
+  const { user } = await getUser({ userId: values.id! })
   if (!user) {
     return {
       success: false,
       message: "User not found",
-    };
+    }
   }
 
-  let hashedPassword: string;
-  hashedPassword = user.password;
+  let hashedPassword: string
+  hashedPassword = user.password
 
   const validData = {
     display_name: values.display_name,
@@ -115,118 +115,115 @@ export const updateUser = async (values: Partial<user>) => {
     role: values.role,
     bio: values.bio,
     theme: values.theme,
-  };
+  }
 
   const dbUser = await db.user.update({
     where: { id: user.id },
     data: { ...validData },
-  });
+  })
   if (!dbUser) {
     return {
       success: false,
       message: "There was an error updating the user.",
       description: "Please try again later.",
-    };
+    }
   }
 
   if (values.isEmailVerified === false && values.email !== user.email) {
-    await sendNewVerificationEmail(dbUser.email);
+    await sendNewVerificationEmail(dbUser.email)
   }
 
-  revalidatePath(`/u/${dbUser.username}`);
-  revalidatePath(`/${dbUser.username}`);
+  revalidatePath(`/u/${dbUser.username}`)
+  revalidatePath(`/${dbUser.username}`)
 
   return {
     success: true,
     data: dbUser,
-  };
-};
+  }
+}
 
 // TODO: Add to sign up process
 export const isUsernameAvailable = async (username: string) => {
-  const dbUser = await getUser({ username });
+  const dbUser = await getUser({ username })
 
   if (!dbUser) {
     return {
       success: false,
       message: "Username is already taken",
-    };
+    }
   }
-};
+}
 
 export const validatePassword = async (userId: string, password: string) => {
-  const { user } = await getUser({ userId });
+  const { user } = await getUser({ userId })
   if (!user) {
     return {
       success: false,
       message: "User not found",
-    };
+    }
   }
 
-  const isValid = await compare(password, user.password);
+  const isValid = await compare(password, user.password)
   if (!isValid) {
     return {
       success: false,
       message: "Password is invalid",
-    };
+    }
   }
 
   return {
     success: true,
-  };
-};
+  }
+}
 
 export const changePassword = async (
   values: z.infer<typeof EditAccountSchema>,
 ) => {
   try {
     try {
-      EditAccountSchema.parse(values);
+      EditAccountSchema.parse(values)
     } catch (error: any) {
       return {
         success: false,
         message: error.message,
-      };
+      }
     }
 
-    const { user } = await validateSession();
+    const { user } = await validateSession()
     if (!user) {
       return {
         success: false,
         message: "User not found",
-      };
+      }
     }
 
-    const dbUser = await getUser({ userId: user.id });
+    const dbUser = await getUser({ userId: user.id })
     if (!dbUser) {
       return {
         success: false,
         message: "User not found",
-      };
+      }
     }
 
-    const isValidPassword = await validatePassword(
-      user.id,
-      values.oldPassword!,
-    );
+    const isValidPassword = await validatePassword(user.id, values.oldPassword!)
     if (!isValidPassword.success) {
       return {
         success: false,
         message: "Old password is invalid",
-      };
+      }
     }
 
     await updateUser({
       id: user.id,
       password: values.newPassword,
-    });
+    })
 
     if (values.logoutFromOtherDevices === true) {
       await db.session.deleteMany({
         where: { userId: user.id },
-      });
+      })
 
-      await createUserSession(user.id);
+      await createUserSession(user.id)
     }
 
     return {
@@ -235,14 +232,14 @@ export const changePassword = async (
       description:
         values.logoutFromOtherDevices === true &&
         "All other devices have been logged out.",
-    };
+    }
   } catch (error: any) {
     return {
       success: false,
       message: error.message,
-    };
+    }
   }
-};
+}
 
 export const resetPassword = async (
   values: z.infer<typeof ResetPasswordSchemaStep2>,
@@ -250,23 +247,23 @@ export const resetPassword = async (
 ) => {
   try {
     try {
-      ResetPasswordSchemaStep2.parse(values);
+      ResetPasswordSchemaStep2.parse(values)
     } catch (error: any) {
       return {
         success: false,
         message: error.message,
-      };
+      }
     }
 
-    const { user } = await getUser({ email });
+    const { user } = await getUser({ email })
     if (!user) {
       return {
         success: false,
         message: "No account with that email found",
-      };
+      }
     }
 
-    const hashedPassword = await hash(values.newPassword, 12);
+    const hashedPassword = await hash(values.newPassword, 12)
 
     await db.user.update({
       where: {
@@ -275,17 +272,17 @@ export const resetPassword = async (
       data: {
         password: hashedPassword,
       },
-    });
+    })
 
     if (values.logoutFromOtherDevices === true) {
       await db.session.deleteMany({
         where: { userId: user.id },
-      });
+      })
     }
 
-    await createUserSession(user.id);
+    await createUserSession(user.id)
 
-    await sendPasswordWasResetEmail(email);
+    await sendPasswordWasResetEmail(email)
 
     return {
       success: true,
@@ -293,14 +290,14 @@ export const resetPassword = async (
       description:
         values.logoutFromOtherDevices &&
         "All other devices have been logged out.",
-    };
+    }
   } catch (error: any) {
     return {
       success: false,
       message: error.message,
-    };
+    }
   }
-};
+}
 
 export const validateResetPasswordCode = async (
   code: string,
@@ -311,70 +308,70 @@ export const validateResetPasswordCode = async (
       code,
       userEmail: email,
     },
-  });
+  })
   if (!resetPasswordQueryResult) {
     return {
       success: false,
       message: "Invalid token",
-    };
+    }
   }
 
   return {
     success: true,
-  };
-};
+  }
+}
 
 export const isEmailVerified = async (email: string) => {
   try {
-    const { user } = await getUser({ email });
+    const { user } = await getUser({ email })
     if (!user) {
       return {
         error: "Account not found",
-      };
+      }
     }
 
     if (user.isEmailVerified === false) {
       return {
         error: "Email not verified",
         key: "email_not_verified",
-      };
+      }
     }
 
     if (user.isEmailVerified === true) {
       return {
         success: "Email is already verified",
-      };
+      }
     }
 
     return {
       success: "Email verified",
-    };
+    }
   } catch (error: any) {
     return {
       success: false,
       message: error.message,
-    };
+    }
   }
-};
+}
 
 // Modify error handling
 export const getSelf = async ({ username }: { username?: string }) => {
-  const { user } = await validateSession();
-  const self = user;
+  const { user } = await validateSession()
+  const self = user
   if (!self || !self.username) {
-    throw new Error("Unauthorized");
+    throw new Error("Unauthorized")
   }
 
   const dbUser = await db.user.findUnique({
     where: username ? { username } : { id: self.id },
-  });
+  })
   if (!dbUser) {
-    throw new Error("User not found");
+    throw new Error("User not found")
   }
 
   if (self.username !== dbUser.username) {
-    throw new Error("Unauthorized");
+    throw new Error("Unauthorized")
   }
 
-  return dbUser;
-};
+  return dbUser
+}
